fcf1d478907780522367dc6a026dfafe8c036a5f
Lim Yi Qin <legenddemonslayer@gmail.com>
2018-07-02 01:03:33 +0800

    Added Pump selector and resources input

---
+++ qc.js
@@ -0,0 +1,9 @@
+//declaration of nachores required for next pump level
+var pumpreq = [10,25,100,150,300,1000,2000,3000,5000];
+
+
+
+//---------------------------------------------------------------
+
+//start of declaration of rate of nachore per hunt
+
